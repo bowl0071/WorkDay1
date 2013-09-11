b44e8a05fe8239ef8ef4e72dcf20ffd108932872
bowl0071 <bowl0071@umn.edu>
2013-09-11 15:25:22 -0500

    Jon Bowles

--- problem1/BMICalc/BMICalc/Form1.vb
+++ problem1/BMICalc/BMICalc/Form1.vb
@@ -3,6 +3,33 @@
 Public Class Form1
 
     Private Sub btnCalcBMI_Click(sender As System.Object, e As System.EventArgs) Handles btnCalcBMI.Click
+        Dim hSt As String
+        Dim wSt As String
+
+        hSt = txtHeight.Text
+        wSt = txtWeight.Text
+
+        Dim h As Double
+        Dim w As Double
+        h = CDbl(hSt)
+        w = CDbl(wSt)
+
+        Dim BMI As Double
+        BMI = (w * 703) / (h ^ 2)
+
+        TextBox1.Text = CStr(BMI)
+
+    End Sub
+
+    Private Sub txtWeight_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtWeight.TextChanged
+
+    End Sub
+
+    Private Sub txtHeight_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtHeight.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
 
     End Sub
 End Class
